2353dd939dbd2b4f822f7d06ef85f8315927cb0e
VirginieSonnet <virginie.sonnet@imev-mer.fr>
2025-12-09 16:43:43 +0000

    add config and update locations

---
+++ config.R
@@ -0,0 +1,14 @@
+# configutation file = links to major repositories to be used within the project
+
+cfg <- list(
+  # ---- project-local paths ----
+  # project = list(
+  #  root      = "~/complex/projects/complex/2025_proj_DTO-Bioflow/PSDtoCarbonExport-short",
+  # ),
+  
+  # ---- project-local paths ----
+  project = list(
+    root      = here::here(),
+    data      = here::here("data")
+  )
+)
